017c21a597f02b75760dfc21b011b530d56831b5
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-01-23 16:07:06 -0300

    alguns exemplos feitos

---
+++ 01-fundamentos/bloco-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/operadoresAritmeticos.js
@@ -0,0 +1,7 @@
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+const perimeter = (2* base) + (2* heigth);
+
+console.log(area);
+console.log(perimeter);
---
+++ 01-fundamentos/bloco-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/variaveis.js
@@ -0,0 +1,9 @@
+const myName = 'Bryan';
+const birthCity = 'Itabirito';
+let birthYear = 2001;
+birthYear = 2030;
+birthCity = 'BH'
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
+
