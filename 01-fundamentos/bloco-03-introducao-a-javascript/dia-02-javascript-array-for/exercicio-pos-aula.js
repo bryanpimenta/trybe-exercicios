be39360407ad417af7cd4d2d14a1512a2fcfc7d9
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-01-24 19:56:22 -0300

    Feito uma parte

---
+++ 01-fundamentos/bloco-03-introducao-a-javascript/dia-02-javascript-array-for/exercicio-pos-aula.js
@@ -0,0 +1,16 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//1 Percorrer o array imprimindo todos os valores dele
+
+for (let index = 0; index < numbers.length; index +=1){
+    console.log(numbers[index])
+}
+
+//2 Somar todos os valores do array e imprimir o resultado
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index +=1){
+    console.log(soma = soma + numbers[index])
+}
+
+console.log(soma)
