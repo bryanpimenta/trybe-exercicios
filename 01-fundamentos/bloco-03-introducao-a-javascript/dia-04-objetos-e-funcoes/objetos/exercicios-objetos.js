39759587cde33e08403fda0fe7a03bca9751e80c
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-01-26 18:59:53 -0300

    Exericios 05 feito

--- 01-fundamentos/bloco-03-introducao-a-javascript/dia-04-objetos-e-funcoes/objetos/exercicios-objetos.js
+++ 01-fundamentos/bloco-03-introducao-a-javascript/dia-04-objetos-e-funcoes/objetos/exercicios-objetos.js
@@ -31,4 +31,9 @@ player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
 //04 Acessar a chave bestInTheWorld e colocar o valor na frase:
 // “A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes”.
 
-console.log('A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes! '+ player['bestInTheWorld'])
+console.log('A jogadora Marta Silva foi eleita a melhor do mundo por '+ player['bestInTheWorld'].length + ' vezes! ')
+
+//05 Acessar a chave medals e imprimir uma mensagem:
+// formato: “A jogadora possui 2 medalhas de ouro e 3 medalhas de prata”.
+
+console.log('A jogadora possui '+ player.medals.golden + ' medalhas de ouro e '+ player.medals.silver + ' medalhas de prata')
