8cdae095f5752dc177de8d652a65616b056bcd33
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-01-26 18:33:09 -0300

    Exercicio 04 feita

--- 01-fundamentos/bloco-03-introducao-a-javascript/dia-04-objetos-e-funcoes/objetos/exercicios-objetos.js
+++ 01-fundamentos/bloco-03-introducao-a-javascript/dia-04-objetos-e-funcoes/objetos/exercicios-objetos.js
@@ -28,4 +28,7 @@ console.log('A jogadora ' + player.nome +' '+ player.lastName + ' tem ' + player
 
 player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
 
-console.table(player);
+//04 Acessar a chave bestInTheWorld e colocar o valor na frase:
+// “A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes”.
+
+console.log('A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes! '+ player['bestInTheWorld'])
