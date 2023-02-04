f2cc36aa31960181914764da78bf5a532dd357f5
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-02-04 12:50:03 -0300

    Exercicio foguete 01

--- 01-fundamentos/bloco-04-javascript-dom-eventos-e-web-storage/dia02-javascript-trabalhando-com-elementos/exercicio.foquete/script.js
+++ 01-fundamentos/bloco-04-javascript-dom-eventos-e-web-storage/dia02-javascript-trabalhando-com-elementos/exercicio.foquete/script.js
@@ -3,3 +3,11 @@ document.querySelector('#elementoOndeVoceEsta');
 const func = () => document.querySelector('#elementoOndeVoceEsta').parentElement.style.color = 'red';
 func();
 
+let modText = document.querySelector('#pai').firstElementChild
+modText.innerText = 'Killjoy é o melhor sentinela do Valorant!'
+
+document.querySelector('#pai').firstElementChild // primeiro elemento filho
+document.querySelector('#elementoOndeVoceEsta').previousElementSibling // elemento irmão anterior
+document.querySelector('#elementoOndeVoceEsta').nextSibling // proximo irmão nó
+document.querySelector('#elementoOndeVoceEsta').nextElementSibling
+document.querySelector('#pai').lastElementChild.previousElementSibling // ultimo elemento irmão e elemento irmão anterior deste
