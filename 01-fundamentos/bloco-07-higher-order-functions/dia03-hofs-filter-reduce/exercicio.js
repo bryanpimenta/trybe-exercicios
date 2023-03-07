bb6f367d09581a3369177ff86fb2d08e552d556f
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-03-07 12:45:19 -0300

    Adicionada função que mostra o país com maior nome da lista

--- 01-fundamentos/bloco-07-higher-order-functions/dia03-hofs-filter-reduce/exercicio.js
+++ 01-fundamentos/bloco-07-higher-order-functions/dia03-hofs-filter-reduce/exercicio.js
@@ -1,6 +1,6 @@
 const {countries} = require('./countries');
 
-const expectedResult = 120797034;
+/* const expectedResult = 120797034; */
 const getPopulation = () => {
   const populationTotal = countries
   .map((item) => item.population)
@@ -9,11 +9,27 @@ return populationTotal;
 }
 console.log(getPopulation());
 
-const expectedResultt = 4311757;
+/* const expectedResultt = 4311757; */
 const getTotalArea = () => {
   const totalArea = countries
-  .map((item) => item.area)
-  .reduce((acc, curr) => acc + curr, 0);
+    .map((item) => item.area)
+    .reduce((acc, curr) => acc + curr, 0);
 return totalArea;
 }
 console.log(getTotalArea());
+
+/* const expectedResult = {
+    name: 'American Samoa',
+    region: 'Oceania',
+    currencies: [{ code: 'USD', name: 'United States Dollar' }],
+    capital: 'Pago Pago',
+    population: 55197,
+    area: 199
+  } */
+const longestName = () => {
+  const longNameCountrie = countries
+    .reduce((acc, curr) => (acc.name.length > curr.name.length) ? acc : curr);
+return longNameCountrie;
+}
+
+  console.log(longestName());
