959817ac47443990b5b9a7abfd654003c41d558b
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-03-07 12:18:12 -0300

    Adicionada função que soma a população dos países

--- 01-fundamentos/bloco-07-higher-order-functions/dia03-hofs-filter-reduce/exercicio.js
+++ 01-fundamentos/bloco-07-higher-order-functions/dia03-hofs-filter-reduce/exercicio.js
@@ -1,2 +1,11 @@
-const countries = require('./countries');
+const {countries} = require('./countries');
 
+const expectedResult = 120797034;
+const getPopulation = () => {
+  const populationTotal = countries
+  .map((item) => item.population)
+  .reduce((acc, curr) => acc + curr);
+  return populationTotal;
+}
+
+console.log(getPopulation());
