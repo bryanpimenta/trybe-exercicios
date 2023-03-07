6672fc38eb02bfa34c2a709b88fa8f0c1ee20ea8
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-03-07 12:25:53 -0300

    Adicionada função que calcula a área total de todos os países

--- 01-fundamentos/bloco-07-higher-order-functions/dia03-hofs-filter-reduce/exercicio.js
+++ 01-fundamentos/bloco-07-higher-order-functions/dia03-hofs-filter-reduce/exercicio.js
@@ -4,8 +4,16 @@ const expectedResult = 120797034;
 const getPopulation = () => {
   const populationTotal = countries
   .map((item) => item.population)
-  .reduce((acc, curr) => acc + curr);
-  return populationTotal;
+  .reduce((acc, curr) => acc + curr, 0);
+return populationTotal;
 }
-
 console.log(getPopulation());
+
+const expectedResultt = 4311757;
+const getTotalArea = () => {
+  const totalArea = countries
+  .map((item) => item.area)
+  .reduce((acc, curr) => acc + curr, 0);
+return totalArea;
+}
+console.log(getTotalArea());
