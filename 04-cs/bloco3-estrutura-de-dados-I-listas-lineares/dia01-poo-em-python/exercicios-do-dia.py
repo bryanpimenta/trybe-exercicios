0ba26dbcf229775c10eff9c9076ce9f5ac798286
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-10-26 19:26:39 -0300

    (cs) bloco03 dia01 exercicio 2 e 3 do dia

--- 04-cs/bloco3-estrutura-de-dados-I-listas-lineares/dia01-poo-em-python/exercicios-do-dia.py
+++ 04-cs/bloco3-estrutura-de-dados-I-listas-lineares/dia01-poo-em-python/exercicios-do-dia.py
@@ -1,4 +1,7 @@
+import collections
+
 # Exercicio 1 - diagrama da TV
+
 
 class TV:
     def __init__(self, tamanho):
@@ -19,7 +22,7 @@ class TV:
         if canal > 0 and canal < 100:
             self.canal = canal
         else:
-            ValueError("Canal inválido")
+            raise ValueError("Canal inválido")
 
     def ligar_desligar(self):
         self.ligada = not self.ligada
@@ -34,3 +37,74 @@ tv.aumentar_volume()
 tv.aumentar_volume()
 tv.aumentar_volume()
 print(tv)
+
+# Exercicio 2 - classe Estatistica
+
+
+class Estatistica:
+    def __init__(self, lista: list[int]):
+        self.lista = lista
+
+    def media(self):
+        return round(sum(self.lista) / len(self.lista), 2)
+
+    def mediana(self):
+        self.lista.sort()
+        if len(self.lista) % 2 == 0:
+            return round((
+                self.lista[len(self.lista) // 2]
+                + self.lista[len(self.lista) // 2 - 1]
+                ) / 2, 2)
+        else:
+            return round(self.lista[len(self.lista) // 2], 2)
+
+    def moda(self):
+        return collections.Counter(self.lista).most_common(1)[0][0]
+
+    def __str__(self):
+        return f"{self.lista}"
+
+
+numbers = Estatistica([1, 4, 2, 3, 4, 5, 6])
+print("Media:", numbers.media())
+print("Mediana:", numbers.mediana())
+print("Moda:", numbers.moda())
+print(numbers)
+
+
+# Exercicio 3 - classe Figuras geometricas
+
+
+class FiguraGeometrica:
+    def __init__(self, area, perimetro):
+        self.area = area
+        self.perimetro = perimetro
+
+    def __str__(self):
+        return f"Area: {self.area} Perimetro: {self.perimetro}"
+
+
+class Quadrado(FiguraGeometrica):
+    def __init__(self, lado):
+        super().__init__(lado ** 2, lado * 4)
+
+
+class Retangulo(FiguraGeometrica):
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+        super().__init__(base * altura, (base + altura) * 2)
+
+
+class Circulo(FiguraGeometrica):
+    def __init__(self, raio):
+        self.raio = raio
+        super().__init__(3.14 * (raio ** 2), 2 * 3.14 * raio)
+
+
+quadrado = Quadrado(2)
+print("Quadrado:", quadrado)
+retangulo = Retangulo(2, 4)
+print("Retangulo:", retangulo)
+circulo = Circulo(6)
+print("Circulo:", circulo)
